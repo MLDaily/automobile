b575dd06a8e3bd473c61d60fb0ebdb2c4a6a70b0
evamy <antriksh.don@gmail.com>
2017-05-07 14:05:58 -0500

    don't remember what I did

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ import numpy as np
 from datetime import datetime, date, time
 
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
@@ -23,10 +24,13 @@ cols = ['symboling', 'normalised-losses', 'make', 'fuel-type',
         'fuel-system', 'bore', 'stroke', 'compression-ratio', 'horsepower',
         'peak-rpm', 'city-mpg', 'highway-mpg', 'price']
 
-file = pd.read_csv('data/train.csv', names=cols)
-# file = file.apply(lambda x: x.fillna(method='pad'), axis=0)
-
+file = pd.read_csv('data/automobile.csv', names=cols)
+file = file[file['price'] != '?']
+file = file.apply(lambda x: x.fillna(method='pad'), axis=0)
+# train = file[cols[:-1]]
 fh = FeatureHasher(n_features=25, input_type="string")
+# Xcat = fh.transform(np.asarray(train.astype(str)))
+
 
 # for i, chunk in enumerate(file):
 #     chunk = chunk.apply(lambda x: x.fillna(method='pad'), axis=0)
@@ -37,15 +41,16 @@ fh = FeatureHasher(n_features=25, input_type="string")
 
 # Train classifier
 # clf = RandomForestRegressor()
-# clf = LinearRegression()
+# clf = LogisticRegression()
+clf = LinearRegression()
 # clf = KNeighborsRegressor()
 # clf = MLPRegressor()
-clf = SVR(kernel='rbf', C=1e3, gamma=0.1)
+# clf = SVR(kernel='rbf', C=1e3, gamma=0.1)
 all_classes = np.array([0, 1])
 
 y_train = file["price"]
 train = file[cols[:-1]]
-train.drop(["normalised-losses"], axis=1, inplace=True)
+# train.drop(["normalised-losses"], axis=1, inplace=True)
 Xcat = fh.transform(np.asarray(train.astype(str)))
 print 'Training'
 clf.fit(Xcat, y_train)
@@ -55,7 +60,7 @@ test_file = pd.read_csv('data/test.csv', names=cols)
 
 test = test_file[cols[:-1]]
 y_test = test_file["price"]
-test.drop(["normalised-losses"], axis=1, inplace=True)
+# test.drop(["normalised-losses"], axis=1, inplace=True)
 X_test = fh.transform(np.asarray(test.astype(str)))
 
 y_pred = clf.predict(X_test)
@@ -63,12 +68,31 @@ y_pred = clf.predict(X_test)
 for i, value in enumerate(y_test):
     print value, y_pred[i]
 
+print clf.residues_
+# print clf.densify()
+# print clf.decision_function(X_test)
 # import matplotlib.pyplot as plt
 
+# flt = ['normalised-losses', 'wheel-base', 'length', 'width', 'height', 'bore',
+#        'stroke', 'compression-ratio', 'horsepower', 'peak-rpm', 'price']
+# inttp = ['symboling', 'curb-weight', 'engine-size', 'city-mpg',
+# 'highway-mpg']
 
-# for col in cols[:-1]:
-#     f = file[[col, 'price']].cumsum()
-#     f.plot(x=col, y='price')
+# print list(file.columns[file.dtypes == np.int64])
+
+# for col in cols:
+#     f = file[[col, 'price']]
+#     if col not in flt and col not in inttp:
+#         f[col].value_counts().plot(x=col, y='price', kind='bar', alpha=1)
+#     else:
+#         f.plot.hist(y=col, alpha=0.2, bins=50)
 #     # f.show()
 #     plt.savefig('images/' + col + '.png')
 #     plt.close()
+
+# ls = ['peak-rpm', 'engine-size', 'curb-weight', 'price']
+# f = file[ls].cumsum()
+
+# f.plot.hist(x='price', alpha=1)
+# plt.savefig('images/' + '-'.join(ls[:-1]) + '.png')
+# plt.close()
